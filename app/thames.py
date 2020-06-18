e407caa1566e44132ce9cd2f6b50bb895dbe80f9
James Tomlinson <tomo.bbe@gmail.com>
2020-06-18 11:03:09 +0100

    Remove local data and download hydrology from Azure blobv store.

--- app/thames.py
+++ app/thames.py
@@ -3,6 +3,7 @@
 from pywr.model import Model
 from pywr.recorders import TablesRecorder
 from pywr.recorders.progress import ProgressRecorder
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import click
 import os
 import logging
@@ -10,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 MODEL_FILENAME = 'model/thames.json'
 OUT_DIR = 'outputs/'
+DATA_DIR = 'data/'
 
 
 @click.group()
@@ -20,6 +22,9 @@ def cli():
 @cli.command()
 def historic_run():
     """Run the model."""
+    logger.info('Initialising cloud storage client ...')
+    client = init_azure_storage()
+    download_hydrology(client)
     logger.info('Starting model run ...')
     # Run the model
     if not os.path.exists(OUT_DIR):
@@ -35,6 +40,28 @@ def historic_run():
     logger.info(stats)
     stats_df = stats.to_dataframe()
     logger.info(stats_df)
+
+
+def download_hydrology(blob_service_client: BlobServiceClient):
+    """Download the hydrology data"""
+
+    filename = 'thames_stochastic_flow.gz'
+    version = 'v1.0.0'
+    download_file_path = os.path.join(DATA_DIR, filename)
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
+    # Create a blob client using the local file name as the name for the blob
+    blob_path = f'hydrology/ref/{version}/{filename}'
+    blob_client = blob_service_client.get_blob_client(container='data', blob=blob_path)
+
+    logger.info(f'Downloading hydrology data from:\n  "{blob_path}"\nto:\n  "{download_file_path}"')
+    with open(download_file_path, "wb") as download_file:
+        download_file.write(blob_client.download_blob().readall())
+
+
+def init_azure_storage() -> BlobServiceClient:
+    connect_str = os.getenv('AZURE_BLOB_CONNECT_STR')
+    return BlobServiceClient.from_connection_string(connect_str)
 
 
 if __name__ == '__main__':
